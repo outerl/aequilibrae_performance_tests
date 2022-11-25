d899ba5c8cd337e8349b3ba4bc0db44bd02ee4d5
Jake Moss <jake.moss@uqconnect.edu.au>
2022-11-25 10:33:45 +1000

    igraph_testing now doesn't require additional args

--- igraph_testing.py
+++ igraph_testing.py
@@ -1,17 +1,26 @@
-import numpy as np
-import timeit
 import igraph as ig
+import numpy as np
+import pandas as pd
+import timeit
 
 
 def igraph_compute_skim(graph, centroids, weights):
+    """
+    Execute the skim using igraph. Weights can either be an array of the
+    corrosponding index to weight or a string to look up the weights in the
+    graph.
+    """
     return np.array(graph.distances(source=centroids, target=centroids, weights=weights, mode="out"))
 
 
-def igraph_init(graph, nodes, cost):
+def igraph_init(graph, cost: str):
+    """
+    Prepare the aequilibrae graph for computation with igraph.
+    """
     # We get the graph
     # NOTE: These are 1-indexed, igraph uses 0-indexing. All names or indexs will be
     # one off
-    # nodes = proj.network.nodes.data
+
     # ig's Graph.DataFrame method assumes the first 2 columns of the `edges` arg is
     # the sources and destination of the edge
     cols = graph.graph.columns.tolist()
@@ -20,15 +29,15 @@ def igraph_init(graph, nodes, cost):
 
     # IDs in the graph need to be a sequence, so can't passing in a indexed version
     # of the graph otherwise igraph will generate a ton of empty vertexes
-    g = ig.Graph.DataFrame(edges, directed=True, vertices=nodes, use_vids=True)
+    g = ig.Graph.DataFrame(edges, directed=True, vertices=pd.DataFrame({"node_id": graph.all_nodes}), use_vids=True)
 
     g.es[cost] = edges[cost].tolist()
 
     centroids = graph.nodes_to_indices[graph.centroids]
-    return g, centroids
+    return g, centroids, cost
 
 
-def igraph_testing(graph, nodes, cost):
+def igraph_testing(graph, nodes, cost: str, iters: int = 2, repeats: int = 5):
     g, centroids = igraph_init(graph, nodes, cost)
     t = timeit.Timer(lambda: igraph_compute_skim(g, centroids, cost))
     times = t.repeat(repeat=repeats, number=iters)

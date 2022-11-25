3c6771005d495afb1b5ebed29e80ea628c081c48
Jake Moss <jake.moss@uqconnect.edu.au>
2022-11-25 10:25:05 +1000

    Add imports to validation, remove global
    Allow path specification for projects. Needs further work not at
    indented functionality

--- validation.py
+++ validation.py
@@ -1,4 +1,16 @@
-def validate(skim1, skim2, atol=1e-01):
+import numpy as np
+import numpy.typing as npt
+from project_utils import project_init
+
+from aeq_testing import aequilibrae_init, aequilibrae_compute_skim
+from igraph_testing import igraph_init, igraph_compute_skim
+
+
+def validate(skim1: npt.ArrayLike, skim2: npt.ArrayLike, atol: float = 1e-01):
+    """
+    Detect discrepencies in the provided skims with a tolerance. Prints the
+    relevant locations, number of unique values, and max absolute difference.
+    """
     isclose = np.isclose(skim1, skim2, atol=atol)
     if isclose.all():
         return True
@@ -12,15 +24,11 @@ def validate(skim1, skim2, atol=1e-01):
         print(f"The max absolute difference is {np.abs(skim1 - skim2).max()}")
 
 
-def validate_all_projects(projects):
+def validate_all_projects(proj_path: str, projects: str, cost: str):
     # Quickly test that both methods produce the same result. FIXME they don't...
     for project_name in projects:
-        graph, nodes = project_init(project_name)
+        graph, nodes = project_init(f"{proj_path}/{project_name}")
 
-        aeq_skim = aequilibrae_compute(aequilibrae_init(graph, cost)).free_flow_time
+        aeq_skim = aequilibrae_compute_skim(aequilibrae_init(graph, cost))[cost]
         ig_skim = igraph_compute_skim(*igraph_init(graph, nodes, cost), cost)
         validate(aeq_skim, ig_skim)
-
-    del graph
-    del nodes
-

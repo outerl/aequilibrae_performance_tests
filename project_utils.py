724bdeeba06d041b366793275dc875280f13d0fd
Jake Moss <jake.moss@uqconnect.edu.au>
2022-11-25 10:32:44 +1000

    Allow project_init to use a file path and other costs and modes

--- project_utils.py
+++ project_utils.py
@@ -1,12 +1,14 @@
 from aequilibrae import Project
 
-def project_init(project_name):
-    proj_path = f"../models/{project_name}/"
 
+def project_init(proj_path: str, cost: str = "free_flow_time", modes: str = "c"):
+    """
+    Opens a aequilibrae project from `proj_path`. Builds to
+    relevant graph and return the graph and nodes.
+    """
     proj = Project()
     proj.open(proj_path)
 
-    proj.network.build_graphs(["free_flow_time"], modes="c")
+    proj.network.build_graphs([cost], modes=modes)
     graph = proj.network.graphs["c"]
     return graph, proj.network.nodes.data
-

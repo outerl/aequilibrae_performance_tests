c7a68b09d83bd1767eccb222ff6665a5224e8df9
chrisobi02 <chrisgobrien02@gmail.com>
2022-11-28 11:58:40 +1000

    Added networkit benchmark file

---
+++ networkit_testing.py
@@ -0,0 +1,35 @@
+import aequilibrae as ae
+import networkit.graph as g
+import networkit.distance as nk
+import numpy as np
+import plotly.express as px
+import timeit
+import warnings
+import pandas as pd
+
+
+def networkit_init(graph: ae.Graph, cost: str):
+    """
+    Initialises the pandana network, executes each individual benchmark
+    """
+    #Setting up the compressed graph to be tested
+    graph.set_graph(cost)
+    net = graph.compact_graph
+    workit = g.Graph(weighted=True, directed=True)
+    for i in list(zip(net["a_node"].tolist(), net["b_node"].tolist(), graph.compact_cost)):
+        workit.addEdge(i[0], i[1], w=i[2], addMissing=True)
+    return workit
+
+
+def networkit_testing(net: g.Graph, centroids):
+    """Tests the equivalent to the aequilibrae skim matrix,
+    computes path for all centroids to all centroids, will output the length of each path in a (long) list"""
+    timer = timeit.Timer(lambda: networkit_compute(net, centroids))
+    times = timer.repeat(repeat=repeats, number=iters)
+    return times
+
+
+def networkit_compute(net: g.Graph, centroids: list):
+    """Only way to do centroid to all"""
+    for i in centroids:
+        dist = nk.Dijkstra(net, i, storePaths=True).run()

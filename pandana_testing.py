145389ff23a43142edfdd037d26eb4b14bd08526
chrisobi02 <chrisgobrien02@gmail.com>
2022-11-28 11:36:23 +1000

    Fixed bug in pandana_testing skim process

--- pandana_testing.py
+++ pandana_testing.py
@@ -13,18 +13,18 @@ def pandana_init(graph: ae.Graph, cost: str):
     net = graph.network
     pnet = pnd.Network(net["link_id"], net["link_id"], net["a_node"], net["b_node"],
                        net[[cost]], twoway=False)
-    centroids = [o for o in graph.centroids for d in graph.centroids]
-    return pnet, centroids
 
-def pandana_testing(net: pnd.Network, centroids):
+    return pnet, [o for o in graph.centroids for d in graph.centroids], [d for o in graph.centroids for d in graph.centroids]
+
+def pandana_testing(net: pnd.Network, orig, dest):
     """Tests the equivalent to the aequilibrae skim matrix,
     computes path for all centroids to all centroids, will output the length of each path in a (long) list"""
-    timer = timeit.Timer(lambda: pandana_compute(net, centroids))
+    timer = timeit.Timer(lambda: pandana_compute(net, orig, dest))
     times = timer.repeat(repeat=repeats, number=iters)
     return times
 
-def pandana_compute(net: pnd.Network, centroids: list):
-    return net.shortest_path_lengths(centroids, centroids)
+def pandana_compute(net: pnd.Network, orig: list, dest: list):
+    return net.shortest_path_lengths(orig, dest)
 
    # net = graph.compact_graph
    # pnet = pnd.Network(net["link_id"], net["link_id"], net["a_node"], net["b_node"],

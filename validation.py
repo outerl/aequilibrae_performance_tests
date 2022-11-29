c9887d3a6ea90b0ae113545a6426a88b5fb7ca68
Jake Moss <jake.moss@uqconnect.edu.au>
2022-11-29 11:41:19 +1000

    Integrate igraph, pandana into validation
    Allow switching of the libraries to be validated.
    Allow switching of the projects used for validation.

--- validation.py
+++ validation.py
@@ -2,10 +2,13 @@
 import numpy as np
 from argparse import ArgumentParser
 import warnings
+from typing import List
 
 from project_utils import project_init
 from aeq_testing import aequilibrae_init, aequilibrae_compute_skim
 from igraph_testing import igraph_init, igraph_compute_skim
+from pandana_testing import pandana_init, pandana_compute
+from networkit_testing import networkit_init, networkit_compute
 
 
 def validate(skim1, skim2, atol: float = 1e-01):
@@ -27,7 +30,7 @@ def validate(skim1, skim2, atol: float = 1e-01):
         return False
 
 
-def validate_all_projects(proj_path: str, projects: str, cost: str, cores: int = 1):
+def validate_projects(proj_path: str, projects: str, libraries: List[str], cost: str, cores: int = 1):
     # Quickly test that both methods produce the same result. FIXME they don't...
     result = True
     for project_name in projects:
@@ -35,28 +38,59 @@ def validate_all_projects(proj_path: str, projects: str, cost: str, cores: int =
 
         graph, nodes = project_init(f"{proj_path}/{project_name}")
 
-        aeq_skim = aequilibrae_compute_skim(*aequilibrae_init(graph, cost)).get_matrix(cost)
-        ig_skim = igraph_compute_skim(*igraph_init(graph, cost))
+        skims = []  # NOTE: the first element is used as the reference model
 
-        if not validate(aeq_skim, ig_skim):
-            result = False
+        for library in libraries:
+            if "aequilibrae" == library:
+                print(f"Running aequilibrae on {project_name}...")
+                skims.append(aequilibrae_compute_skim(*aequilibrae_init(graph, cost)).get_matrix(cost))
+            elif "igraph" == library:
+                print(f'Running igraph on {project_name}...')
+                skims.append(igraph_compute_skim(*igraph_init(graph, cost)))
+            elif "pandana" == library:
+                print(f"Running pandana on {project_name}...")
+                skims.append(np.array(pandana_compute(*pandana_init(graph, cost))).reshape((24,24)))
+            # elif "networkit" == library:
+            #     print(f"Running Networkit on {project_name}...")
+            #     skims.append(networkit_compute(*networkit_init(graph, cost)))
+
+        print("")
+        for skim, library in list(zip(skims, libraries))[1:]:
+            if not validate(skims[0], skim):
+                print(f"{library} differs from {libraries[0]}")
+                result = False
+            else:
+                print(f"{library} matches {libraries[0]}")
     return result
 
 
 def main():
     projects = ["sioux_falls", "chicago_sketch"]
+    libraries = ["aequilibrae", "igraph", "pandana", "networkit"]
     cost = "free_flow_time"
 
     parser = ArgumentParser()
     parser.add_argument("-m", "--model-path", dest="path", default='../models',
                         help="path to models", metavar="FILE")
+    parser.add_argument("-l", "--libraries", nargs='+', dest="libraries",
+                        choices=libraries,
+                        default=libraries,
+                        help="libraries to validate, the first one is used as the reference")
+    parser.add_argument("-p", "--projects", nargs='+', dest="projects",
+                        choices=projects,
+                        default=projects,
+                        help="projects to benchmark using")
 
     args = vars(parser.parse_args())
 
     with warnings.catch_warnings():
         # pandas future warnings are really annoying FIXME
         warnings.simplefilter(action="ignore", category=FutureWarning)
-        return validate_all_projects(args["path"], projects, cost)
+        if validate_projects(args["path"], args["projects"], args["libraries"], cost):
+            print("All models validated")
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":

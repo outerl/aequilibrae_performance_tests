5a1291b7bb69edd946ef59764762df84bfea4513
Jake Moss <jake.moss@uqconnect.edu.au>
2022-11-29 13:57:15 +1000

    Style changes

--- pandana_testing.py
+++ pandana_testing.py
@@ -1,10 +1,7 @@
 import aequilibrae as ae
 import pandana as pnd
-import numpy as np
-import plotly.express as px
 import timeit
-import warnings
-import pandas as pd
+
 
 def pandana_init(graph: ae.Graph, cost: str):
     """
@@ -16,19 +13,14 @@ def pandana_init(graph: ae.Graph, cost: str):
 
     return pnet, [o for o in graph.centroids for d in graph.centroids], [d for o in graph.centroids for d in graph.centroids]
 
-def pandana_testing(net: pnd.Network, orig, dest):
+
+def pandana_testing(net: pnd.Network, orig, dest, iters: int = 2, repeats: int = 5):
     """Tests the equivalent to the aequilibrae skim matrix,
     computes path for all centroids to all centroids, will output the length of each path in a (long) list"""
     timer = timeit.Timer(lambda: pandana_compute(net, orig, dest))
     times = timer.repeat(repeat=repeats, number=iters)
     return times
 
+
 def pandana_compute(net: pnd.Network, orig: list, dest: list):
     return net.shortest_path_lengths(orig, dest)
-
-   # net = graph.compact_graph
-   # pnet = pnd.Network(net["link_id"], net["link_id"], net["a_node"], net["b_node"],
-  #                     pd.DataFrame(graph.compact_cost), twoway=False)
-  #  timer = timeit.Timer(lambda: pandana_pathes(pnet, centroids, centroids))
-  #  times = timer.repeat(repeat=repeat, number=iterz)
-  #  return timesz

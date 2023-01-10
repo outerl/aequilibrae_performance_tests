de01e6094b51e112d4305ecd79e1d1c428133c6d
chrisobi02 <chrisgobrien02@gmail.com>
2023-01-10 11:42:06 +1000

    added plot for impact of cores on a given project

--- Images/graph_script.py
+++ Images/graph_script.py
@@ -33,25 +33,62 @@ def normalised_time_plot(summary: pd.DataFrame, projects: pd.DataFrame) -> go.Fi
         'yanchor': 'top'},
           xaxis_title="Number of Edges in Graph",
           yaxis_title="Seconds",
-          legend_title="Algorithms",
+          legend_title="Data Structure",
         font=dict(family="Arial", size=18,),
                   paper_bgcolor='rgba(0,0,0,0)',
                   plot_bgcolor='rgba(0,0,0,0)'
             )
     return fig
+
+def core_plot(summary: pd.DataFrame, projects: pd.DataFrame, proj: str, normalised=True):
+    """Does the normalised core plot for one project"""
+    # ratio of performance between aeq and a designated library (comparison), based on the size of the network (num edges).
+    fig = go.Figure()
+    cores = list(set(summary.index.get_level_values("cores")))
+    cores.sort()
+    for alg in set(summary.index.get_level_values("details")):
+        norm_time = []
+        for core in cores:
+            try:
+                norm_time.append((summary.loc[(proj, alg, core)]["min"] / projects.loc[proj, "num_centroids"]) * 1000) \
+                    if normalised else norm_time.append(summary.loc[(proj, alg, core)]["min"])
+                print(alg, core, norm_time[-1])
+            except:
+                pass
+        trace = go.Scatter(x=cores, y=norm_time, name=alg)
+        fig.add_trace(trace)
+        # Iterate through each algorithm, then for each project compute the ratio
+        # Aesthetics
+        #print("library: ", alg, ", ratio: ", norm_time)
+    fig.update_layout(title={
+        'text': "Minimum Runtime per 1000 Origins for "+ proj,
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'},
+        xaxis_title="Number of Cores",
+        yaxis_title="Seconds",
+        legend_title="Data Structure",
+        font=dict(family="Arial", size=18, ),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)'
+    )
+    return fig
+
 #Create the mega dataframe, link the script to the file path for the boys, francois csv
 francois1 = pd.read_csv(r"C:\Users\61435\Downloads\Arkansas_francois.csv")
 francois2 = pd.read_csv(r"C:\Users\61435\Downloads\longAn_Chicago_sf_francois.csv")
 boys = pd.read_csv(r"C:\Users\61435\Downloads\longAn_Chicago_sf_boys.csv")
 all_data = pd.concat([francois1,francois2,boys])
-#agg by core count
-core_agg = all_data.groupby("cores")
+proj_summary = pd.read_csv(r"C:\Users\61435\Downloads\project summary (1).csv", index_col=0).drop("sioux_falls", axis=0)
+
 #plots by Core
+core_data = all_data.groupby(["project_name", "details", "cores"]).min()
+core_plot(core_data, proj_summary, "LongAn", normalised=False).show()
 
 #normalised time plot
-proj_summary = pd.read_csv(r"C:\Users\61435\Downloads\project summary (1).csv", index_col=0).drop("sioux_falls", axis=0)
-time_data = core_agg.get_group(1).groupby(["project_name","details"]).min().drop("cores", axis=1)
-normalised_time_plot(time_data, proj_summary).show()
+# time_data = all_data.groupby("cores").get_group(1).groupby(["project_name","details"]).min().drop("cores", axis=1)
+# normalised_time_plot(time_data, proj_summary).show()
 
 
 

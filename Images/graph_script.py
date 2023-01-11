e176d9277519cde65aab1aaeb095f343a3a666f7
chrisobi02 <chrisgobrien02@gmail.com>
2023-01-11 11:21:02 +1000

    added sql integration

--- Images/graph_script.py
+++ Images/graph_script.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import plotly.graph_objects as go
 import math
-def normalised_time_plot(summary: pd.DataFrame, projects: pd.DataFrame) -> go.Figure:
+import sqlite3 as sq
+
+def normalised_time_plot(summary: pd.DataFrame, projects: pd.DataFrame, normalised=True) -> go.Figure:
     """
 
     :param summary: Benchmark results
@@ -13,21 +15,27 @@ def normalised_time_plot(summary: pd.DataFrame, projects: pd.DataFrame) -> go.Fi
     """
     #ratio of performance between aeq and a designated library (comparison), based on the size of the network (num edges).
     fig = go.Figure()
+    projs = list(set(summary.index.get_level_values("project_name")))
+    projs.sort(key=lambda x: projects.loc[x, "num_links"])
+    print(projs)
     for alg in set(summary.index.get_level_values("details")):
         norm_time = []
-        for proj in set(summary.index.get_level_values("project_name")):
+        for proj in projs:
             try:
-                norm_time.append((summary.loc[(proj, alg)]["min"] / projects.loc[proj, "num_centroids"]) * 1000)
-                print(alg, proj, norm_time[-1])
+                #print(proj)
+                norm_time.append((summary.loc[(proj, alg)]["min"] / projects.loc[proj, "num_centroids"]) * 1000) \
+                    if normalised else norm_time.append(summary.loc[(proj, alg)]["min"])
+                #print(alg, proj, norm_time[-1])
             except:
+                #print(proj, " failed")
                 pass
         trace = go.Scatter(x=projects["num_links"], y=norm_time, name=alg)
         fig.add_trace(trace)
     #Iterate through each algorithm, then for each project compute the ratio
         #Aesthetics
-        print("library: ", alg, ", ratio: ", norm_time)
+        #print("library: ", alg, ", ratio: ", norm_time)
     fig.update_layout(title={
-        'text': "Minimum Runtime per 1000 Origins",
+        'text': "Minimum Runtime per 1000 Origins" if normalised else "Minimum Runtime",
         'y': 0.9,
         'x': 0.5,
         'xanchor': 'center',
@@ -41,12 +49,13 @@ def normalised_time_plot(summary: pd.DataFrame, projects: pd.DataFrame) -> go.Fi
             )
     return fig
 
-def core_plot(summary: pd.DataFrame, projects: pd.DataFrame, proj: str, normalised=True, transformation=None):
+def core_plot(summary: pd.DataFrame, projects: pd.DataFrame, proj: str, normalised=True, transformation=None, xtransform=None):
     """Does the normalised core plot for one project"""
     # ratio of performance between aeq and a designated library (comparison), based on the size of the network (num edges).
     fig = go.Figure()
     cores = list(set(summary.index.get_level_values("cores")))
     cores.sort()
+    #print([xtransform(x) for x in cores])
     for alg in set(summary.index.get_level_values("details")):
         norm_time = []
         for core in cores:
@@ -54,12 +63,13 @@ def core_plot(summary: pd.DataFrame, projects: pd.DataFrame, proj: str, normalis
                 if transformation is None:
                     norm_time.append((summary.loc[(proj, alg, core)]["min"] / projects.loc[proj, "num_centroids"]) * 1000) \
                         if normalised else norm_time.append(summary.loc[(proj, alg, core)]["min"])
-                    print(alg, core, norm_time[-1])
+                    #print(alg, core, norm_time[-1])
                 else:
                     norm_time.append(transformation(summary.loc[(proj, alg, core)]["min"]))
             except:
                 pass
-        trace = go.Scatter(x=cores, y=norm_time, name=alg)
+        trace = go.Scatter(x=cores if xtransform is None else [xtransform(x) for x in cores],
+                           y=norm_time, name=alg)
         fig.add_trace(trace)
         # Iterate through each algorithm, then for each project compute the ratio
         # Aesthetics
@@ -80,35 +90,37 @@ def core_plot(summary: pd.DataFrame, projects: pd.DataFrame, proj: str, normalis
     )
     return fig
 
-#Create the mega dataframe, link the script to the file path for the boys, francois csv
-francois1 = pd.read_csv(r"C:\Users\61435\Downloads\Arkansas_francois.csv")
-francois2 = pd.read_csv(r"C:\Users\61435\Downloads\longAn_Chicago_sf_francois.csv")
-boys = pd.read_csv(r"C:\Users\61435\Downloads\longAn_Chicago_sf_boys.csv")
-all_data = pd.concat([francois1,francois2,boys])
-proj_summary = pd.read_csv(r"C:\Users\61435\Downloads\project summary (1).csv", index_col=0).drop("sioux_falls", axis=0)
+def csv():
+    francois1 = pd.read_csv(r"C:\Users\61435\Downloads\Arkansas_francois.csv")
+    francois2 = pd.read_csv(r"C:\Users\61435\Downloads\longAn_Chicago_sf_francois.csv")
+    boys = pd.read_csv(r"C:\Users\61435\Downloads\longAn_Chicago_sf_boys.csv")
+    all_data = pd.concat([francois1, francois2, boys])
+    return all_data
 
-#plots by Core
-core_data = all_data.groupby(["project_name", "details", "cores"]).min()
-core_plot(core_data, proj_summary, "LongAn").show()
+def sql():
+    data = sq.connect(r"C:\Users\61435\Downloads\results_threadripper.sqlite")
+    cur = data.cursor()
+    sql_data = pd.read_sql('SELECT * FROM results', data)
+    df = pd.DataFrame(sql_data, columns=["runtime", "project_name", "cores", "details"])
+    df.rename(columns={"runtime": "min"}, inplace =True)
+    return df
 
-#normalised time plot
-# time_data = all_data.groupby("cores").get_group(1).groupby(["project_name","details"]).min().drop("cores", axis=1)
-# normalised_time_plot(time_data, proj_summary).show()
+CSV = False
+if __name__ == "__main__":
+    # Create the mega dataframe, link the script to the file path for the boys, francois csv
+    if CSV:
+        all_data = csv()
+        time_data = all_data.groupby("cores").get_group(1).groupby(["project_name", "details"]).min()\
+            .drop("cores", axis=1).drop("sioux_falls", axis=0)
+    else:
+        all_data = sql()
+        time_data = all_data.groupby("cores").get_group(1).groupby(["project_name", "details"]).min().drop("sioux_falls", axis=0)
 
+    proj_summary = pd.read_csv(r"C:\Users\61435\Downloads\project summary (1).csv", index_col=0).drop("sioux_falls",
+                                                                                                      axis=0)
+    # plots by Core
+    core_data = all_data.groupby(["project_name", "details", "cores"]).min()
+    core_plot(core_data, proj_summary, "Australia").show()
 
-
-    # df = pd.read_csv("C:\Users\61435\Downloads\heap_benchmark_combined.csv")
-    # df.drop("computer", axis=1)
-    # summary = df.groupby(["project_name", "library"]).agg(
-    #             average=("runtime", "mean"), min=("runtime", "min"), max=("runtime", "max")
-    #         ).drop("sioux_falls")
-    # proj_summary = pd.read_csv("C:\Users\61435\Downloads\project summary (1).csv", index_col=0).drop("sioux_falls", axis=0)
-    # #b, c = proj_summary.iloc[1].copy(), proj_summary.iloc[2].copy()
-    # #proj_summary.iloc[1],proj_summary.iloc[2] = c,b
-    #
-    # #    largest_proj = proj_summary["num_nodes"].idxmax()
-    # #aeq_ratios(summary, proj_summary, summary.loc[largest_proj, "min"].idxmin(),
-    #            #["aequilibrae", "igraph", "networkit", "graph-tool"]).write_image("C:\Users\61435\Desktop\aequilibrae_performance_tests\Images\Benchmark_ratios.png", width=1000, height=800)
-    # #benchmark_chart(summary, ["chicago_sketch", "Arkansas statewide model", "LongAn"], ["aequilibrae", "graph-tool", "igraph", "networkit"],["Chicago Sketch", "Arkansas Statewide Model", "Long An"]).write_image(r"C:\Users\61435\Desktop\aequilibrae_performance_tests\Images\Benchmark_proj.png", width=1000, height=800)
-    # #time_plot(summary, proj_summary, ["aequilibrae", "igraph", "networkit", "graph-tool"]).write_image("Images/link_time.png",  width=1000, height=800)
-    # normalised_time_plot(summary, proj_summary, ["aequilibrae", "igraph", "networkit", "graph-tool"])#.write_image("Images/normedlink_time.png",  width=1000, height=800)
+    # normalised time plot
+    # normalised_time_plot(time_data, proj_summary, normalised=False).show()
